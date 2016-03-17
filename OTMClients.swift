a06cec0cd15de8124f61c4c25cf41de70ee0c088
josephdhooper <jdhooper@gmail.com>
2016-03-17 16:27:42 -0400

    Add delete session.

--- OTMClients.swift
+++ OTMClients.swift
@@ -158,6 +158,26 @@ func submitStudentInformation(mapString: String, mediaURL: String, placemark: MK
     }
     task.resume()
 }
+    func logout ()
+    {
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        request.HTTPMethod = "DELETE"
+        var xsrfCookie: NSHTTPCookie? = nil
+        let sharedCookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        for cookie in sharedCookieStorage.cookies as [NSHTTPCookie]! {
+            if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+        }
+        if let xsrfCookie = xsrfCookie {
+            request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
+        }
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+        }
+        task.resume()
+    }
 
     class func escapedParameters(parameters: [String : AnyObject]) -> String {
         
