8817fa9bde7ff886df40b9c97acda61dfe6befdc
josephdhooper <jdhooper@gmail.com>
2016-03-26 11:13:44 -0400

    Fix keyboard methods.

--- On The Map/OTMPinViewController.swift
+++ On The Map/OTMPinViewController.swift
@@ -131,8 +131,8 @@ class OTMPinViewController: UIViewController, MKMapViewDelegate, UITextFieldDele
     
     // MARK: - Keyboard methods
     func subscribeToKeyboardNotifications() {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(OTMPinViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(OTMPinViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
     }
     
     func unsubscribeFromKeyboardNotifications() {
